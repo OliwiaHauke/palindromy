6b1b68b6cf015af5f1899cd60ec5a6e25c611e53
Oliwia Hauke <oliwia.urbanska.95@gmail.com>
2025-01-20 10:13:18 +0100

    palindromy.py

---
+++ palindromy.py
@@ -0,0 +1,8 @@
+def palindrom(word):
+    return word == word[::-1]
+
+print(palindrom("potop"))
+print(palindrom("krowa"))
+print(palindrom("kajak"))
+print(palindrom("zaraz"))
+print(palindrom("potop"))
