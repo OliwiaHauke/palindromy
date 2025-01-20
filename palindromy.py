d4ee030a80ae3b3b4caf28bafe04b9f664fe18f1
Oliwia Hauke <oliwia.urbanska.95@gmail.com>
2025-01-20 10:20:31 +0100

    Added documentation

--- palindromy.py
+++ palindromy.py
@@ -1,5 +1,15 @@
 def palindrom(word):
     return word == word[::-1]
+"""
+Checks if the given word is a palindrom.
+
+Parameter:
+word(str): the word to check
+
+Returns:
+bool: True if the word is a palindrome, False otherwise.
+
+"""
 
 print(palindrom("potop"))
 print(palindrom("krowa"))
